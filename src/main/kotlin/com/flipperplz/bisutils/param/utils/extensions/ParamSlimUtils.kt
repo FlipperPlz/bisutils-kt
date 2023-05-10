cce593203ef494410db836eb6d180657a40a0468
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-05-10 13:35:18 -0700

    fix: Overloaded Function Name In ParamSlimUtils

--- src/main/kotlin/com/flipperplz/bisutils/param/utils/extensions/ParamSlimUtils.kt
+++ src/main/kotlin/com/flipperplz/bisutils/param/utils/extensions/ParamSlimUtils.kt
@@ -12,57 +12,57 @@ import java.nio.ByteBuffer
 inline fun RapElement?.createValue(value: () -> String): RapString =
     ParamSlimStringImpl(this, value())
 
-inline operator fun ParamSlimArrayImpl.set(i: Int, element: RapLiteralBase) { slimValue[i] = element }
+operator fun ParamSlimArrayImpl.set(i: Int, element: RapLiteralBase) { slimValue[i] = element }
 
-inline fun createValue(parent: RapElement? = null, value: () -> String): RapString =
+inline fun createRapValue(parent: RapElement? = null, value: () -> String): RapString =
     ParamSlimStringImpl(parent, value())
 
 inline fun RapElement?.createValue(value: () -> Int): RapInt =
     ParamSlimIntImpl(this, value())
 
-inline fun createValue(parent: RapElement? = null, value: () -> Int): RapInt =
+inline fun createRapValue(parent: RapElement? = null, value: () -> Int): RapInt =
     ParamSlimIntImpl(parent, value())
 
 inline fun RapElement?.createValue(value: () -> Float): RapFloat =
     ParamSlimFloatImpl(this, value())
 
-inline fun createValue(parent: RapElement? = null, value: () -> Float): RapFloat =
+inline fun createRapValue(parent: RapElement? = null, value: () -> Float): RapFloat =
     ParamSlimFloatImpl(parent, value())
 
 inline fun RapElement?.createValue(value: () -> MutableList<RapLiteralBase>): RapArray =
     ParamSlimArrayImpl(this, value())
 
-inline fun createValue(parent: RapElement? = null, value: () -> MutableList<RapLiteralBase>): RapArray =
+inline fun createRapValue(parent: RapElement? = null, value: () -> MutableList<RapLiteralBase>): RapArray =
     ParamSlimArrayImpl(parent, value())
 
 inline operator fun <reified T: RapNamedElement> RapStatementHolder.get(name: String): T? =
     slimCommands.filterIsInstance<T>().firstOrNull {it.slimName.equals(name, true) }
 
-inline operator fun RapStatementHolder.get(name: String): RapStatement? =
+operator fun RapStatementHolder.get(name: String): RapStatement? =
     slimCommands.firstOrNull {it is RapNamedElement && it.slimName.equals(name, true) }
 
-inline operator fun RapStatementHolder.rem(name: String): RapClass =
+operator fun RapStatementHolder.rem(name: String): RapClass =
     slimCommands.filterIsInstance<RapClass>().first { it.slimName.equals(name, true) }
 
 //TODO(RYANN): order should be made, first deletes then classes then variables then preprocessor shit
-inline infix fun RapStatementHolder.contains(name: String): Boolean =
+infix fun RapStatementHolder.contains(name: String): Boolean =
     get(name) == null
 
-inline infix fun RapStatementHolder.contains(command: RapStatement): Boolean =
+infix fun RapStatementHolder.contains(command: RapStatement): Boolean =
     slimCommands.contains(command)
 
 inline fun <reified T: RapStatement> RapStatementHolder.childrenOfType(): List<T> =
     slimCommands.filterIsInstance<T>()
 
-inline fun RapStatementHolder.childClasses(): List<RapClass> = childrenOfType()
+fun RapStatementHolder.childClasses(): List<RapClass> = childrenOfType()
 
-inline operator fun RapArray.get(index: Int): RapLiteralBase? =
+operator fun RapArray.get(index: Int): RapLiteralBase? =
     slimValue?.get(index)
 
-inline operator fun RapStatementHolder.iterator(): Iterator<RapStatement> =
+operator fun RapStatementHolder.iterator(): Iterator<RapStatement> =
     slimCommands.iterator()
 
-inline operator fun RapArray.iterator(): Iterator<RapLiteralBase> =
+operator fun RapArray.iterator(): Iterator<RapLiteralBase> =
     slimValue?.iterator() ?: iterator {  }
 
 operator fun RapLiteralBase.invoke(buffer: ByteBuffer, parent: RapElement?): RapLiteralBase? = when(buffer.get()) {
@@ -72,6 +72,3 @@ operator fun RapLiteralBase.invoke(buffer: ByteBuffer, parent: RapElement?): Rap
     3.toByte() -> RapArrayImpl(buffer, parent)
     else -> null
 }
-
-// DELEGATION
-
