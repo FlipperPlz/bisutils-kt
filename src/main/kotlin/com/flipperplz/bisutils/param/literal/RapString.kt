205b9f855ee074d5d3bd0b8283753d540aa09cde
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-05-11 04:25:06 -0700

    feat(Param): Remove charSequence Extension

--- src/main/kotlin/com/flipperplz/bisutils/param/literal/RapString.kt
+++ src/main/kotlin/com/flipperplz/bisutils/param/literal/RapString.kt
@@ -3,21 +3,12 @@ package com.flipperplz.bisutils.param.literal
 import com.flipperplz.bisutils.param.node.RapLiteral
 import com.flipperplz.bisutils.param.utils.ParamLiteralTypes
 
-interface RapString : RapLiteral<String>, CharSequence {
+interface RapString : RapLiteral<String> {
     override val literalId: Byte?
         get() = 0
 
     override val slimLiteralType: ParamLiteralTypes
         get() = ParamLiteralTypes.STRING
-
-    override val length: Int
-        get() = slimValue?.length ?: 0
-
-    override operator fun get(index: Int): Char =
-        slimValue?.get(index) ?: 0x0.toChar()
-
-    override fun subSequence(startIndex: Int, endIndex: Int): CharSequence =
-        slimValue?.subSequence(startIndex, endIndex) ?: ""
 
     override fun toParam(): String =
         if (slimValue != null) "\"${slimValue!!.replace("\"", "\"\"")}\"" else "//Unknown String"
