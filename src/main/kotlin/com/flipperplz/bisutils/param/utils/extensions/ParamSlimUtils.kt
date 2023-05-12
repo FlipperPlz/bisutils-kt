2e00d4a6725de65eebe421dad2e4248f19bf7f8c
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-05-11 18:21:08 -0700

    feat(param): Reimplement ParamUtils createRapVariable functions

--- src/main/kotlin/com/flipperplz/bisutils/param/utils/extensions/ParamSlimUtils.kt
+++ src/main/kotlin/com/flipperplz/bisutils/param/utils/extensions/ParamSlimUtils.kt
@@ -1,37 +1,69 @@
 package com.flipperplz.bisutils.param.utils.extensions
 
 import com.flipperplz.bisutils.param.literal.RapArray
+import com.flipperplz.bisutils.param.literal.RapFloat
+import com.flipperplz.bisutils.param.literal.RapInt
+import com.flipperplz.bisutils.param.literal.RapString
 import com.flipperplz.bisutils.param.node.*
 import com.flipperplz.bisutils.param.statement.RapClass
-import java.nio.ByteBuffer
+import com.flipperplz.bisutils.param.utils.ParamStringType
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
 
-//inline fun RapElement?.createValue(value: () -> String): RapString =
-//    MutableRapStringImpl(this, value())
-//
+inline fun RapElement?.createValue(crossinline value: () -> String): RapString =
+    object : RapString {
+        override var slimStringType: ParamStringType = ParamStringType.QUOTED
+        override var slimValue: String? = value()
+        override val slimParent: RapElement = this
+    }
+
 //operator fun MutableRapArrayImpl.set(i: Int, element: RapLiteralBase) {
 //    slimValue[i] = element
 //}
 //
-//inline fun createRapValue(parent: RapElement? = null, value: () -> String): RapString =
-//    MutableRapStringImpl(parent, value())
-//
-//inline fun RapElement?.createValue(value: () -> Int): RapInt =
-//    MutableRapIntImpl(this, value())
-//
-//inline fun createRapValue(parent: RapElement? = null, value: () -> Int): RapInt =
-//    MutableRapIntImpl(parent, value())
-//
-//inline fun RapElement?.createValue(value: () -> Float): RapFloat =
-//    MutableRapFloatImpl(this, value())
-//
-//inline fun createRapValue(parent: RapElement? = null, value: () -> Float): RapFloat =
-//    MutableRapFloatImpl(parent, value())
-//
-//inline fun RapElement?.createValue(value: () -> MutableList<RapLiteralBase>): RapArray =
-//    MutableRapArrayImpl(this, value())
-//
-//inline fun createRapValue(parent: RapElement? = null, value: () -> MutableList<RapLiteralBase>): RapArray =
-//    MutableRapArrayImpl(parent, value())
+inline fun createRapValue(parent: RapElement? = null, crossinline value: () -> String): RapString =
+    object : RapString {
+        override var slimStringType: ParamStringType = ParamStringType.QUOTED
+        override var slimValue: String? = value()
+        override val slimParent: RapElement? = parent
+    }
+
+inline fun RapElement?.createValue(crossinline value: () -> Int): RapInt =
+    object : RapInt {
+        override val slimValue: Int = value()
+        override val slimParent: RapElement = this
+    }
+
+inline fun createRapValue(parent: RapElement? = null, crossinline value: () -> Int): RapInt =
+    object : RapInt {
+        override val slimValue: Int = value()
+        override val slimParent: RapElement? = parent
+    }
+
+inline fun RapElement?.createValue(crossinline value: () -> Float): RapFloat =
+    object : RapFloat {
+        override val slimValue: Float = value()
+        override val slimParent: RapElement = this
+    }
+
+inline fun createRapValue(parent: RapElement? = null, crossinline value: () -> Float): RapFloat =
+    object : RapFloat {
+        override val slimValue: Float = value()
+        override val slimParent: RapElement? = parent
+    }
+
+inline fun RapElement?.createValue(crossinline value: () -> List<RapLiteralBase>): RapArray =
+    object : RapArray {
+        override val slimValue: List<RapLiteralBase> = value()
+        override val slimParent: RapElement = this
+    }
+
+inline fun createRapValue(parent: RapElement? = null, crossinline value: () -> List<RapLiteralBase>): RapArray =
+    object : RapArray {
+        override val slimValue: List<RapLiteralBase> = value()
+        override val slimParent: RapElement? = parent
+    }
 
 inline operator fun <reified T : RapNamedElement> RapStatementHolder.get(name: String): T? =
     slimCommands.filterIsInstance<T>().firstOrNull { it.slimName.equals(name, true) }
