fe85f59c7c44a3590145971cc22a6a7974e7aa8d
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-05-31 14:01:22 -0700

    fix(bankUtils): fix ParamSlimUtils::mutableStringTable

--- src/main/kotlin/com/flipperplz/bisutils/bank/utils/BankUtils.kt
+++ src/main/kotlin/com/flipperplz/bisutils/bank/utils/BankUtils.kt
@@ -31,17 +31,14 @@ class BankProcessor(
     private var currentEntry: PboDataEntry? = null //current entry being preprocessed
     private val configFiles: MutableMap<PboDataEntry, ParamFile> = mutableMapOf()
     private val stringTables: MutableMap<PboDataEntry, StringTableFile> = mutableMapOf() //TODO: stringtables implementation
-
     private val globalConfig: ParamMutableFile = mutableParamFile("config")
     private val globalStringTable: StringTableMutableFile = mutableStringTable()
 
     val banks: List<PboFile> = _banks
-
     fun loadBank(bank: PboFile) {
         _banks.add(bank)
         indexBank(bank)
     }
-
     fun process(flush: Boolean = true): List<PboFile> {
         val banks = mutableListOf<PboFile>()
         //TODO: Process Banks
@@ -102,13 +99,13 @@ class BankProcessor(
     private fun locateEnforceFile(include: EnforceIncludeDirective, encoding: Charset = Charsets.UTF_8): String =
         locateVFSEntry(PboFile.normalizePath(include.path) ?: throw Exception("Error normalizing PBO path"))?.entryData?.array()?.toString(encoding) ?: throw EnforceIncludeNotFoundException(include)
 
+    private fun locateVFSEntry(path: String): PboDataEntry? {
+        TODO("Locate entry in vfs")
+    }
+
     private fun flush() {
         _boostPreprocessor.flush(); _banks.clear()
         stringTables.clear(); globalStringTable.flush()
         configFiles.clear(); globalConfig.flush()
-    }
-
-    private fun locateVFSEntry(path: String): PboDataEntry? {
-        TODO("Locate entry in vfs")
     }
 }
--- src/main/kotlin/com/flipperplz/bisutils/stringtable/astImpl/mutable/StringTableMutableFileImpl.kt
+++ src/main/kotlin/com/flipperplz/bisutils/stringtable/astImpl/mutable/StringTableMutableFileImpl.kt
@@ -6,6 +6,6 @@ import com.flipperplz.bisutils.stringtable.ast.mutable.StringTableMutableFile
 import com.flipperplz.bisutils.stringtable.util.StringTableFormat
 
 data class StringTableMutableFileImpl(
-    override val languages: MutableList<StringTableLanguage>,
+    override val languages: MutableList<StringTableLanguage> = mutableListOf(),
     override val localizations: MutableList<StringTableLocalization> = mutableListOf()
 ) : StringTableMutableFile, MutableList<StringTableLocalization> by localizations
